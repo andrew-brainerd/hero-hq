8cbcda551dfe4979fac92875ccd8445d97263832
Andrew Brainerd <drwb333@gmail.com>
2022-12-04 23:26:35 -0500

    Add search to downloads

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -1,13 +1,38 @@
-import { useContext } from 'preact/hooks';
+import { useContext, useEffect, useState } from 'preact/hooks';
+import cn from 'classnames';
 import HeroContext from '../../context';
+import useDebounce from '../../hooks/useDebounce';
 import Song from '../Song/Song';
 
 const Download = () => {
   const { downloadableSongs } = useContext(HeroContext);
+  const [filter, setFilter] = useState('');
+  const [songList, setSongList] = useState(downloadableSongs);
+  const debouncedFilter = useDebounce(filter, 750);
+
+  useEffect(() => {
+    const formattedFilter = filter.toLocaleLowerCase();
+    const updatedSongList = (downloadableSongs || []).filter(
+      song =>
+        song.artist.toLocaleLowerCase().includes(formattedFilter) ||
+        song.track.toLocaleLowerCase().includes(formattedFilter)
+    );
+    setSongList(updatedSongList);
+  }, [downloadableSongs, debouncedFilter]);
 
   return (
     <div className={'download'}>
-      {downloadableSongs.length ? downloadableSongs.map(songData => <Song {...songData} />) : <h1>No Downloads</h1>}
+      <div className={cn('row', 'search')}>
+        <input
+          type="text"
+          placeholder={'Song or Artist'}
+          onChange={e => setFilter((e.target as HTMLInputElement).value)}
+          value={filter}
+        />
+      </div>
+      <div className={'song-list'}>
+        {songList.length ? songList.map(songData => <Song {...songData} />) : <h1>No Downloads</h1>}
+      </div>
     </div>
   );
 };
