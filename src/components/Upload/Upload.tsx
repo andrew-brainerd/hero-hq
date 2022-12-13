e1e6cca88955bde732b52f3ff5d276bfce1cadb1
Andrew Brainerd <drwb333@gmail.com>
2022-12-12 22:52:00 -0500

    Migrate from Preact to React

--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'preact/hooks';
+import { useContext, useEffect, useState } from 'react';
 import cn from 'classnames';
 import HeroContext from '../../context';
 import Song from '../Song/Song';
